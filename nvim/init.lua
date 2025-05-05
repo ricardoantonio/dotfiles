1fb402b2739857d32e686ad957d3982df7c5bac5
Ricardo Antonio <ricardoantoniomx@gmail.com>
2025-05-05 17:19:38 -0600

    feat(nvim): migrate to LazyVim as base configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,2 @@
---[[
-
-  ::::    ::: ::::::::::  ::::::::  :::     ::: ::::::::::: ::::     ::::
-  :+:+:   :+: :+:        :+:    :+: :+:     :+:     :+:     +:+:+: :+:+:+
-  :+:+:+  +:+ +:+        +:+    +:+ +:+     +:+     +:+     +:+ +:+:+ +:+
-  +#+ +:+ +#+ +#++:++#   +#+    +:+ +#+     +:+     +:+     +#+  +:+  +#+
-  +#+  +#+#+# +#+        +#+    +#+  +#+   +#+      +#+     +#+       +#+
-  #+#   #+#+# #+#        #+#    #+#   #+#+#+#       #+#     #+#       #+#
-  ###    #### ##########  ########      ###     ########### ###       ###
-
-  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-                      My Personal Neovim Configuration
-  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  Credits & Inspiration:
-  - kickstart.nvim (https://github.com/nvim-lua/kickstart.nvim)
-  - LazyVim (https://www.lazyvim.org)
-
---]]
-require 'config'
-
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/config/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require('config.options')
-require('config.mappings')
-require('config.autocmds')
-require('config.lazy')
