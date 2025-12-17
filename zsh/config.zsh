474a8516e4ea539df5826f31ae83efa6aed11250
Ricardo Antonio <ricardoantoniomx@gmail.com>
2025-12-16 19:06:24 -0600

    feat(terminal): alias for yazi

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -35,5 +35,14 @@ source <(fzf --zsh)
 # ---- zoxide ----
 eval "$(zoxide init zsh --cmd cd)"
 
+# ---- yazi ----
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
 # ---- Starship Promp ----
 eval "$(starship init zsh)"
