385ffdc69d33dc26ba38af127877f3803f93b2e2
Ricardo Antonio <git@ricardoantonio.com>
2026-02-11 07:49:30 -0600

    feat: removes djlint for go templates

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,25 +1,9 @@
 return {
   {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "djlint",
-      },
-    },
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        gotmpl = { "djlint" },
-      },
-      formatters = {
-        djlint = {
-          prepend_args = {
-            "--profile=golang",
-            "--indent=2",
-          },
-        },
+        gotmpl = { "prettier" },
       },
     },
   },
